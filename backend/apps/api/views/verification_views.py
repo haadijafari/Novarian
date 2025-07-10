5b10e641de910612f9a3c71884397b13ebe50e19
Hadi Jafari <haadijafari2003@gmail.com>
2025-07-10 16:19:53 +0330

    [FIX] Merge Verification URLs
    [+] Create identifier verification URL
    [-] Remove email verification URL
    [-] Remove phone number verification URL

--- backend/apps/api/views/verification_views.py
+++ backend/apps/api/views/verification_views.py
@@ -14,51 +14,43 @@ from apps.utils.verification import send_email_verification_code, send_phone_ver
 
 User = get_user_model()
 
-class VerifyEmailAPIView(APIView):
+class VerifyAPIView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        email = request.data.get('email')
+        identifier = request.data.get('identifier')
         code = request.data.get('code')
 
-        if not email or not code:
-            return Response({'detail': 'Email and code are required.'}, status=status.HTTP_400_BAD_REQUEST)
+        if not identifier or not code:
+            return Response(
+                {'detail': 'Identifier (Email or Phone Number) and code are required.'}, 
+                status=status.HTTP_400_BAD_REQUEST
+                )
+        
+        if '@' in identifier:
+            cached_code = cache.get(f'verify_email:{identifier}')
+        else:
+            cached_code = cache.get(f'verify_phone:{identifier}')
 
-        cached_code = cache.get(f'verify_email:{email}')
         if cached_code != code:
             return Response({'detail': 'Invalid or expired code.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        user = User.objects.filter(email=email).first()
+        if '@' in identifier:
+            user = User.objects.filter(email=identifier).first()
+        else:
+            user = User.objects.filter(phone_number=identifier).first()
+
         if user:
-            user.is_verified_email = True
-            user.save(update_fields=['is_verified_email'])
-            cache.delete(f'verify_email:{email}')
-            return Response({'message': 'Email verified successfully.'}, status=status.HTTP_202_ACCEPTED)
+            if '@' in identifier:
+                user.is_verified_email = True
+            else:
+                user.is_verified_phone_number = True
+
+            user.save()
+            cache.delete(f'verify_{'email' if '@' in identifier else 'phone'}:{identifier}')
+            return Response({'message': 'Identifier verified successfully.'}, status=status.HTTP_202_ACCEPTED)
         return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
 
-
-class VerifyPhoneAPIView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        phone = request.data.get('phone_number')
-        code = request.data.get('code')
-
-        if not phone or not code:
-            return Response({'detail': 'Phone number and code are required.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        cached_code = cache.get(f'verify_phone:{phone}')
-        if cached_code != code:
-            return Response({'detail': 'Invalid or expired code.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        user = User.objects.filter(phone_number=phone).first()
-        if user:
-            user.is_verified_phone_number = True
-            user.save(update_fields=['is_verified_phone_number'])
-            cache.delete(f'verify_phone:{phone}')
-            return Response({'message': 'Phone number verified successfully.'}, status=status.HTTP_202_ACCEPTED)
-        return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
-    
 
 class ResendVerificationCodeAPIView(APIView):
     def post(self, request):
