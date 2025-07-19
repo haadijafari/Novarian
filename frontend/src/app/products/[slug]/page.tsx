9b632642f7217b1b0f6c2a7512a74d883c45597d
bahmanik <bahmaniali350@gmail.com>
2025-07-19 12:42:54 +0000

    [FIX] Displaying diffrent gallery layout for lg and sm
    [*] changing the layout of [slug]/page.tsx based on tailwind media quiries

--- frontend/src/app/products/[slug]/page.tsx
+++ frontend/src/app/products/[slug]/page.tsx
@@ -4,15 +4,14 @@ import React from 'react'
 const page = () => {
   return (
     <div className='flex flex-col'>
+      <div className='flex items-center h-screen w-screen bg-fuchsia-600  p-4 flex-col'>
+        <div>test</div>
 
-      <div className='flex items-center h-screen w-screen bg-fuchsia-600 lg:grow justify-center'>
-        <div className='flex lg:shrink-0 p-2 lg:h-auto lg:basis-5xl bg-orange-400'>
-
-          <div className="flex flex-3 bg-green-400 flex-col lg:pl-15 md:pl-4">
+        <div className='flex w-full max-w-7xl p-2 md:flex-row flex-col-reverse bg-orange-400 rounded-lg'>
+          <div className="flex flex-[3] bg-green-400 flex-col md:pl-4">
             <div className='bg-indigo-400 flex-1'>
               ksjdl
             </div>
-
             <div className='bg-violet-600 flex-1'>
               sdwee
             </div>
--- frontend/src/components/products/ImageGallery.tsx
+++ frontend/src/components/products/ImageGallery.tsx
@@ -2,30 +2,31 @@ import Image from 'next/image'
 import React from 'react'
 
 const ImageGallery = () => {
+  const ImageArray = ["/1.jpg", "/2.jpg", "/3.jpg", "/4.jpg"]
   return (
     <>
-      {/* image div */}
-      < div className="flex flex-4 bg-cyan-100 flex-col" >
-        <div className=''>
-          <Image src="/1.jpg" alt="image" width={1000} height={1000} />
+      <div className="flex flex-4 bg-cyan-100 flex-col" >
+        {/*mobile*/}
+        <div className='sm:hidden flex'>
+          <Image className='rounded-3xl' src="/1.jpg" alt="image" width={1000} height={1000} />
         </div>
 
-        {/* light box div */}
-        <div className='bg-red-700 flex flex-1'>
-          <div className="flex-1 bg-pink-300">
-            <Image src="/1.jpg" alt="image" width={1000} height={1000} />
+        {/*desktop*/}
+        <div className='sm:block hidden'>
+          <div>
+            <Image className='rounded-3xl' src="/1.jpg" alt="image" width={1000} height={1000} />
           </div>
-          <div className="flex-1 bg-pink-400">
-            <Image src="/1.jpg" alt="image" width={1000} height={1000} />
+
+          {/* light box div */}
+          <div className='flex flex-1 gap-3 m-2 mt-4'>
+            {ImageArray.map((e, i) => {
+              return <div key={i} className="flex-1">
+                <Image className="rounded-2xl" src={e} alt="image" width={1000} height={1000} />
+              </div>
+            })}
           </div>
-          <div className="flex-1 bg-pink-500">
-            <Image src="/1.jpg" alt="image" width={1000} height={1000} />
-          </div>
-          <div className="flex-1 bg-pink-600">
-            <Image src="/1.jpg" alt="image" width={1000} height={1000} />
-          </div>
-        </div>
-      </div >
+        </div >)
+      </div>
     </>
 
   )
