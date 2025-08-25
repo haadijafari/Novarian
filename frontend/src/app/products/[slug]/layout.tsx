e2f41b074e11d92ba8f2ee8d4841254292108926
bahmanik <bahmaniali350@gmail.com>
2025-08-25 13:12:46 +0000

    [ADD] Footer component

--- frontend/src/app/products/[slug]/layout.tsx
+++ frontend/src/app/products/[slug]/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/navbar/productNavbar' // Updated import path
 import ProductAccordion from '@/components/products/productAccordion'
 import { ProductBreadCrumb } from '@/components/products/productBreadCrumb'
+import Footer from '@/components/ui/Footer'
 import { ReactNode } from 'react'
 
 type Props = { children: ReactNode, modals: ReactNode, productHero: ReactNode, reviews: React.ReactNode, suggestions: ReactNode, discussions: ReactNode }
@@ -8,13 +9,12 @@ type Props = { children: ReactNode, modals: ReactNode, productHero: ReactNode, r
 export default function Layout({ children, modals, productHero, reviews, suggestions, discussions }: Props) {
 
   return (
-    <div className="flex flex-col h-screen w-full overflow-y-auto dark:bg-navy-900">
+    <div className="flex flex-col dark:bg-navy-900">
       <Navbar
         brandText={"اسم-محصول"}
       />
       <div className="mx-2.5 flex-none transition-all md:pr-2 xl:ml-[323px]">
         {children}
-        <ProductBreadCrumb />
         <div className='flex items-center justify-center min-h-screen w-screen bg-surface p-4'>
           {productHero}
         </div>
@@ -29,6 +29,7 @@ export default function Layout({ children, modals, productHero, reviews, suggest
           {discussions}
         </div>
       </div>
+      <Footer />
       {modals}
     </div>
   );
---
+++ frontend/src/components/ui/Footer.tsx
@@ -0,0 +1,60 @@
+'use client'
+import { useScroll, useTransform, motion, type MotionValue, useSpring } from 'framer-motion'
+import { useRef } from 'react'
+
+export default function Footer() {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start end', 'end end']
+  })
+
+  const smoothScrollYProgress = useSpring(scrollYProgress, {
+    stiffness: 80,
+    damping: 20,
+    restDelta: 0.001
+  })
+
+  return (
+    <div className="relative" ref={containerRef}>
+      <svg className="w-full mb-40" viewBox="0 0 250 90">
+        <path fill="none" id="curve" d="m0,88.5c61.37,0,61.5-68,126.5-68,58,0,51,68,123,68" />
+        <text className="text-[4px] uppercase fill-surface-accent">
+          {
+            [...Array(3)].map((_, i) => {
+              const startOffset = useTransform(
+                smoothScrollYProgress, [0, 1],
+                [`${145 - (i * 45)}%`, `${100 - (i * 45)}%`]
+              )
+
+              return (
+                <motion.textPath
+                  key={i}
+                  direction="rtl"
+                  startOffset={startOffset}
+                  href="#curve">
+                  همانطور که هر هدیه داستانی دارد، ما راوی داستان کسب‌وکار شما خواهیم بود.
+                </motion.textPath>
+              )
+            })
+          }
+        </text>
+      </svg>
+      <StaticLists scrollProgress={scrollYProgress} />
+    </div>
+  )
+}
+
+const StaticLists = ({ scrollProgress }: { scrollProgress: MotionValue<number> }) => {
+  const y = useTransform(scrollProgress, [0, 1], [-225, 0])
+
+  return (
+    <div className="h-[250px] bg-surface-muted overflow-hidden">
+      <motion.div style={{ y }} className="h-full flex justify-center items-center p-10">
+        <ol className="flex justify-center text-ink">
+          place holder for footers footer
+        </ol>
+      </motion.div>
+    </div>
+  )
+}
