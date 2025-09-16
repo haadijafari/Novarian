97b6e6904dcb7680dd09e990ace4f78f1fda1a18
bahmanik <bahmaniali350@gmail.com>
2025-09-16 21:13:22 +0330

    [FIX] Navbar color

--- frontend/src/components/navbar/product-navbar/ProductNavbar.tsx
+++ frontend/src/components/navbar/product-navbar/ProductNavbar.tsx
@@ -10,7 +10,7 @@ const Navbar = ({ brandText }: Props) => {
     <nav className="sticky top-4 z-40 flex flex-row flex-wrap items-center justify-between rounded-xl bg-white/10 p-2 backdrop-blur-xl dark:bg-[#0b14374d]">
       <NavbarBrand brandText={brandText} />
 
-      <div className="relative mt-[3px] flex h-[61px] w-[355px] flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-[365px] md:flex-grow-0 md:gap-1 xl:w-[365px] xl:gap-2">
+      <div className="relative mt-[3px] flex h-[61px] w-[355px] flex-grow items-center justify-around gap-2 rounded-full bg-surface-muted px-2 py-2 shadow-xl shadow-shadow-500 md:w-[365px] md:flex-grow-0 md:gap-1 xl:w-[365px] xl:gap-2">
         <NavbarSearch />
         <NotificationsDropdown />
         <ThemeToggle />
